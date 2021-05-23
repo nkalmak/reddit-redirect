2ae891425c8566f9d0426c52c80dfb3bd4613fa2
Nicholas <nkalmak@gmail.com>
2021-05-23 01:06:51 -0700

    changed script from onload to document_start

--- contentScript.js
+++ contentScript.js
@@ -1,16 +1,14 @@
-window.onload = () => {
-  chrome.storage.sync.get(['setUrl'], (e) => {
-    if (!e.setUrl) {
-      var newUrl;
-      do {
-        newUrl = prompt('Enter a full URL to redirect reddit to (include https://) :')
-      } while (!newUrl.startsWith('http'));
-      chrome.storage.sync.set({ 'setUrl': newUrl }, () => {
-        console.log('Settings saved');
-      });
-      window.location = newUrl;
-    } else {
-      window.location = e.setUrl;
-    };
-  });
-}
+chrome.storage.sync.get(['setUrl'], (e) => {
+  if (!e.setUrl) {
+    var newUrl;
+    do {
+      newUrl = prompt('Enter a full URL to redirect reddit to (include https://) :')
+    } while (!newUrl.startsWith('http'));
+    chrome.storage.sync.set({ 'setUrl': newUrl }, () => {
+      console.log('Settings saved');
+    });
+    window.location = newUrl;
+  } else {
+    window.location = e.setUrl;
+  };
+});
