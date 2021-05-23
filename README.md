563aca656fc8572192eede991125096b11e69bf6
Nicholas <nkalmak@gmail.com>
2021-05-23 00:30:30 -0700

    app complete

---
+++ README.md
@@ -0,0 +1,3 @@
+# Must use full URL (include https://)
+
+# To clear saved URL, remove and re-load extension
---
+++ contentScript.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+  chrome.storage.sync.get(['setUrl'], (e) => {
+    if (e.setUrl == undefined) {
+      var newUrl;
+      do {
+        newUrl = prompt('Enter a full URL to redirect reddit to (include https://) :')
+      } while (!newUrl.startsWith('http'));
+      chrome.storage.sync.set({ 'setUrl': newUrl }, () => {
+        console.log('Settings saved');
+      });
+      window.location = newUrl;
+    } else {
+      window.location = e.setUrl;
+    };
+  });
+}
