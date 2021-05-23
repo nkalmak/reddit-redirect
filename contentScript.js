2af745b8a2eed06fd53a11c17343e898f30272af
Nicholas <nkalmak@gmail.com>
2021-05-23 11:27:08 -0700

    code cleanup

--- contentScript.js
+++ contentScript.js
@@ -2,13 +2,13 @@ chrome.storage.sync.get(['setUrl'], (e) => {
   if (!e.setUrl) {
     var newUrl;
     do {
-      newUrl = prompt('Enter a full URL to redirect reddit to (include https://) :')
+      newUrl = prompt('Enter a full URL to redirect reddit to (include https://) :');
     } while (!newUrl.startsWith('http'));
     chrome.storage.sync.set({ 'setUrl': newUrl }, () => {
       console.log('Settings saved');
-    });
+    })
     window.location = newUrl;
   } else {
     window.location = e.setUrl;
-  };
-});
+  }
+})
